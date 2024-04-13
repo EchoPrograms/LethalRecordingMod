d6f3050e0b9f2f9a58771c8359643da13fba7cda
EchoPrograms <brogan.oberhaus30@gmail.com>
2024-04-12 22:47:56 -0400

    initial commit

---
+++ MyFirstPlugin/Plugin.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+
+namespace MyFirstPlugin
+{
+    [BepInPlugin("TheNerds", "LethalRecordingMod", "0.0.1")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin TheNerds.LethalRecordingMod is loaded!");
+        }
+    }
+}
