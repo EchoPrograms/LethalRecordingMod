51d72d6d09ec71e4a573289f49341d16646567ed
Chboo1 <chboo.1.chickadoo@gmail.com>
2024-04-13 17:17:39 -0400

    Added base class Troll and base class IntervalTroll

--- LethalRecordingMod/Plugin.cs
+++ LethalRecordingMod/Plugin.cs
@@ -1,25 +1,114 @@
-﻿using BepInEx;
+﻿// Alter's steam ID: 76561198073630548
+using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
 
+
 namespace LethalRecordingMod
 {
+	[BepInPlugin("TheNerds.LethalRecordingMod", "LethalRecordingMod", "0.0.1")]
+	public class Plugin : BaseUnityPlugin
+	{
+		private readonly Harmony harmony = new Harmony("TheNerds.LethalRecordingMod");
+		public static Plugin Instance;
+		public ManualLogSource mls;
+		private void Awake()
+		{
+			if (Instance == null) {
+				Instance = this;
+			} 
+			// Plugin startup logic
+			Logger.LogInfo($"Plugin LethalRecordingMod hath become loaded!");
+			mls.LogInfo("We're up and running!");
+		}
+	}
 
-   
-    [BepInPlugin("TheNerds.LethalRecordingMod", "LethalRecordingMod", "0.0.1")]
-    public class Plugin : BaseUnityPlugin
-    {
-        private readonly Harmony harmony = new Harmony("TheNerds.LethalRecordingMod");
-        public static Plugin Instance;
-        public ManualLogSource mls;
-        private void Awake()
-        {
-            if (Instance == null) {
-                Instance = this;
-            } 
-            // Plugin startup logic
-            Logger.LogInfo($"Plugin LethalRecordingMod hath become loaded!");
-            mls.LogInfo("We're up and running!");
-        }
-    }
+
+	enum TrollSettingType
+	{
+		Undefined,
+		Toggle,
+		Value,
+		Person,
+		IGT, // In-game time
+		RLT // Real-life time
+	}
+
+
+	public class TrollSettingValue
+	{
+		TrollSettingType type;
+		bool toggle;
+		double val;
+		long person;
+		double IGT; // 0-1, 0 = start of the day, 1 = midnight
+		byte RLT_month; // Don't know why you'd want this
+		byte RLT_mday; // Day of the month
+		byte RLT_wday; // Day of the week
+		byte RLT_hour; // Hour
+		byte RLT_minute; // Minute
+		byte RLT_second; // Second, optional
+		short RLT_millisecond; // Millisecond, optional
+	}
+
+
+	public class TrollSetting
+	{
+		public string name = "Name your settings, guys!";
+		public TrollSettingValue val;
+		public TrollSetting(TrollSettingType type, string newName)
+		{
+			val.type = type;
+			name = newName;
+		}
+	}
+
+
+	public class Troll // Base class
+	{
+		public:
+		bool repeatable = true;
+		int timesOccured = 0;
+		bool clientSide = false;
+		long targetSteamID;
+		bool active;
+		List<TrollSetting> settings;
+		public int OnTrigger() {} // Should be overwritten by most trolls, returns 0 on success, should be used instead of Start or Awake to be toggleable
+		public int OnStop() {}
+		public int WhileActive(double deltaTime) {} // Should be used instead of Update, to make troll able to be disabled
+		public ref TrollSetting GetSetting(string name)
+		{
+			for (int i = 0; i < settings.Count; i++)
+			{
+				if (settings[i].name == name)
+				{
+					return ref settings[i];
+				}
+			}
+		}
+	}
+	public class IntervalTroll : Troll // Still base class
+	{
+		public static const int defaultInterval = -1; // Seconds
+		public int timer = 0;
+		public static const double defaultChance = 100; // Percentage
+		public IntervalTroll()
+		{
+			timer = 0;
+			settings.Add(new TrollSetting(TrollSettingType.Value, "Interval"));
+			settings.Add(new TrollSetting(TrollSettingType.Value, "Chance"));
+		}
+		public override int WhileActive(double deltaTime)
+		{
+			timer += deltaTime;
+			if (timer >= GetSetting("Interval").val.val)
+			{
+				timer = 0;
+				if ((new Random()).Next(0, 100) < GetSetting("Chance").val.val)
+				{
+					OnTrigger();
+				}
+			}
+		}
+	}
 }
