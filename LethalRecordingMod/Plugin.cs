7b621c64f3a0b84e1ae9a85c9c8b8f2f9d2bb467
agmas <Maxlion360@gmail.com>
2024-04-13 23:41:32 +0400

    Fix mod GUID, create Instance&mls&Harmony, gitignore csproj (hopefully)

--- LethalRecordingMod/Plugin.cs
+++ LethalRecordingMod/Plugin.cs
@@ -1,15 +1,25 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
 
 namespace LethalRecordingMod
 {
-    [BepInPlugin("TheNerds", "LethalRecordingMod", "0.0.1")]
+
+   
+    [BepInPlugin("TheNerds.LethalRecordingMod", "LethalRecordingMod", "0.0.1")]
     public class Plugin : BaseUnityPlugin
     {
+        private readonly Harmony harmony = new Harmony("TheNerds.LethalRecordingMod");
+        public static Plugin Instance;
+        public ManualLogSource mls;
         private void Awake()
         {
+            if (Instance == null) {
+                Instance = this;
+            } 
             // Plugin startup logic
             Logger.LogInfo($"Plugin LethalRecordingMod hath become loaded!");
-            Logger.LogInfo(">:(");
+            mls.LogInfo("We're up and running!");
         }
     }
 }
